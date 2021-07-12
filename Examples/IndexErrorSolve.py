c3bde10ed56b22668a9ed6f566518a1eb767beeb
Andreawthederp <81652601+andrewthederp@users.noreply.github.com>
2021-07-12 20:54:24 +0200

    Create IndexErrorSolve.py
    IndexError

---
+++ Examples/IndexErrorSolve.py
@@ -0,0 +1,9 @@
+# are you getting an IndexError and is your code looking like this?
+
+some_list = ['h','good','error']
+print(some_list[3])
+
+# then do something like this
+
+some_list = ['h','good','error']
+print(some_list[2])
